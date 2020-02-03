7e12aa175d8cb68cf50bd02e52031377e045fba3
Eric Wong <ericwong12@live.com>
2020-02-03 15:27:48 -0700

    inquirer question prompt made

--- app.js
+++ app.js
@@ -2,7 +2,35 @@
 const inquirer = require("inquirer");
 //npm install --save-dev jest (Jest TEST)
 //File Calls
-const employee = require("./lib/Employee")
-const engineer = require("./lib/Engineer")
-const intern = require ("./lib/Intern")
-const manager = require ("./lib/Manager")
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require ("./lib/Intern");
+const manager = require ("./lib/Manager");
+
+//Inquire questions
+
+function employeeName(){
+    const employeeName = inquirer.prompt({
+        type: "input",
+        name: "employeeName",
+        message: "What is your name?" 
+    });
+}
+function employeeId(){
+    const employeeId = inquirer({
+        type: "input",
+        name: "employeeId",
+        message: "Please enter your employee number?"
+    });
+}
+function employeeTitle(){
+    const employeeTitle = inquirer({
+        type: "list",
+        name: "employeeTitle",
+        message: "What is your role in this company?",
+        choices: ["Manager","Engineer","Intern"]
+
+    });
+}
+
+async function 
